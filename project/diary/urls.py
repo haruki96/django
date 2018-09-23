5f5a654f99531e6960970d4ce8351108c9ab5717
haruki96 <haruki150714@gmail.com>
2018-09-23 20:02:42 +0900

    update

--- project/diary/urls.py
+++ project/diary/urls.py
@@ -5,5 +5,8 @@ app_name = 'diary'
 
 urlpatterns = [
 	path('', views.index, name='index'),   #diary/
-	path('add/', views.add, name='add')     #diary/add
+	path('add/', views.add, name='add'),     #diary/add
+	path('update/<int:pk>/', views.update, name='update'), #diary/update/1
+	path('delete/<int:pk>/', views.delete, name='delete'), #diaary/delete/1
+	path('detail/<int:pk>/', views.detail, name='detail'), #diary/detail/1
 	]
--- project/diary/views.py
+++ project/diary/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import DayCreateForm
+from .models import Day
 
 # Create your views here.
 def index(request):
-	return render(request, 'diary/day_list.html') #第二引数はどのページを見せるか
+	context = {
+		'day_list':Day.objects.all()   #databaseに保存されているDayをすべて保存
+	}
+	return render(request, 'diary/day_list.html', context) #第二引数はどのページを見せるか
 
 
 def add(request):
@@ -20,3 +24,51 @@ def add(request):
 		'form':form
 	}
 	return render(request, 'diary/day_form.html', context)
+
+
+def update(request, pk):
+	#urlのpkをもとに、Dayを取得
+	day = get_object_or_404(Day, pk=pk)
+
+	#フォームに、取得したDayを紐づける
+	form = DayCreateForm(request.POST or None, instance=day)
+
+	#method=POST、つまり送信ボタン押下時、入力内容が間違いなければ
+	if request.method == 'POST' and form.is_valid():
+		form.save()
+		return redirect('diary:index')
+
+	#通常時のページアクセスや、入力内容に誤りがあればまたページを表示
+	context = {
+		'form':form
+	}
+	return render(request, 'diary/day_form.html', context)
+
+
+
+def delete(request, pk):
+	#urlのpkをもとに、Dayを取得
+	day = get_object_or_404(Day, pk=pk)
+
+	
+	#method=POST、つまり送信ボタン押下時、入力内容が間違いなければ
+	if request.method == 'POST' :
+		day.delete()                    #モデルのインスタンス.delete
+		return redirect('diary:index')
+
+	#通常時のページアクセスや、入力内容に誤りがあればまたページを表示
+	context = {
+		'day':day,
+	}
+	return render(request, 'diary/day_confirm_delete.html', context)
+
+
+def detail(request, pk):
+	#urlのpkをもとに、Dayを取得
+	day = get_object_or_404(Day, pk=pk)
+
+	#通常時のページアクセスや、入力内容に誤りがあればまたページを表示
+	context = {
+		'day':day,
+	}
+	return render(request, 'diary/day_detail.html', context)
